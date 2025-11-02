545b89bc2a5463b7d3e2aafc577c14d60d06af89
Zexk <bouraoui@outlook.it>
2025-11-02 02:58:07 +0100

    hello world

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+     systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3
+            pkgs.uv
+          ];
+        };
+      });
+    };
+}

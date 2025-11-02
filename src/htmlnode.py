1434d25f6e3f14dbafe8e47bd76ed7b4053fabe1
Zexk <bouraoui@outlook.it>
2025-11-02 05:59:25 +0100

    added ParentNode class with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -32,3 +32,20 @@ class LeafNode(HTMLNode):
 
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("all parent nodes must have a tag")
+        if self.children is None:
+            raise ValueError("all parent nodes must have a child")
+        children_string = ""
+        for child in self.children:
+            children_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{children_string}</{self.tag}>"
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"

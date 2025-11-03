d13d98ecad034d9b90e5d19216e1e7c9a5b532eb
Zexk <bouraoui@outlook.it>
2025-11-03 03:29:18 +0100

    added content generation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
-from textnode import TextNode
+import os
+import shutil
+
+from util import copy_files_recursive
+from generate import generate_page_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-    tn = TextNode("hi mom", 2)
-    print(tn)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+    print("Generating content...")
+    generate_page_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
